c5379666d06e1b584ba5fb3b815b6afb8535de1c
Renlin Li <renlin.li@gmail.com>
2017-05-15 23:17:57 +0100

    binary search

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+import numpy as np
+array = np.random.randint (0, 100, size=10)
+array.sort ()
+
+def binary_search (array, key):
+'''O(log(N))'''
+	hi_index = len (array) - 1
+	lo_index = 0
+	while hi_index > lo_index:
+		mid = (hi_index + lo_index) / 2
+		if array[mid] == key:
+			print "found at %d" % (mid)
+			break
+		elif key < array[mid]:
+			hi_index = mid
+		elif key > array[mid]:
+			lo_index = mid
+
+binary_search (array, array[5])
+
