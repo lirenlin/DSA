d20615c2276bb0bd48504d9fd73f82ba6e02a835
Renlin Li <renlin.li@gmail.com>
2017-06-03 15:23:38 +0100

    update

--- permutation_sequcence.py
+++ permutation_sequcence.py
@@ -2,22 +2,24 @@ def solution (n, k):
   array = [i for i in range(1, n+1)]
   result = list ()
   factorial = 1
+  k -= 1
 
   for i in range (1, n):
     factorial *= i
 
-  while k >= 1:
+  while k >= 0:
     index = k // factorial
-    print index
-    print array
     result.append (array[index])
     array = array[:index] + array[index+1:]
     k %= factorial
+    if not array:
+      break
     factorial /= len (array)
   
-  print result 
+  return result
 
 
 print solution (3, 3)
-#print solution (4, 5)
+print solution (4, 5)
+print solution (4, 2)
 
