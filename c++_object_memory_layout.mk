813037da37da16c98f373deea9def6600965fa37
Renlin Li <renlin.li@gmail.com>
2017-07-23 15:03:44 +0100

    add object memory layout article

---
+++ c++_object_memory_layout.mk
@@ -0,0 +1,2 @@
+http://www.drdobbs.com/cpp/multiple-inheritance-considered-useful/184402074
+https://stackoverflow.com/questions/28521242/memory-layout-of-a-class-under-multiple-or-virtual-inheritance-and-the-vtables
