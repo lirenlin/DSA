944383d9bf76342da67b4f63d71f0a6bb6524179
Renlin Li <renlin.li@gmail.com>
2017-10-23 23:39:57 +0100

    update

--- split_word.py
+++ split_word.py
@@ -1,3 +1,5 @@
+import collections
+
 def wordBreak (string, wordDict):
     n = len (string)
     dp = [0] * (n + 1)
@@ -16,6 +18,37 @@ def wordBreak (string, wordDict):
 
     return dp[n]
 
+def wb_recursive (string, wordDict):
+  dic = collections.defaultdict(list)
+
+  # return the list of word for string s
+  def dfs (s):
+    if not s:
+      return [None]
+
+    #if we already visited this string
+    if s in dic:
+      return dic[s]
+
+    res = list ()
+    for word in wordDict:
+      n = len (word)
+        # if the string start with word
+      if word == s[:n]:
+        for r in dfs (s[n:]):
+          if r:
+            res.append (word + " " + r)
+          else:
+            res.append (word)
+
+    dic[s] = res
+    return res
+
+  return dfs (string)
+
 string = "Iwanttoworkinshanghai"
-wordDict = {"I", "want", "to", "work", "in", "shang" "hai", "shanghai", "wantto"}
+wordDict = ("I", "want", "to", "work", "in", "shang" "hai", "shanghai", "wantto")
 print wordBreak (string, wordDict)
+
+res_list = wb_recursive (string, wordDict)
+print min (res_list, key = len)
