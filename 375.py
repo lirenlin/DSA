26bfc9417d2132ed12bdb48474840d82d8c54079
Renlin Li <renlin.li@gmail.com>
2017-10-29 22:58:16 +0000

    add

--- 375.py
+++ 375.py
@@ -1,45 +1,55 @@
-class Solution (object):
-  def getMoneyAmount(self, n):
-    def result (dp, start, end):
-      if start >= end:
-        return 0
+import sys
 
-      if dp[start][end] != -1:
-        return dp[start][end]
+def getMoneyAmount (N):
+  # number of guess
+  dp_dict = dict ()
+  dp = [-1 for i in range (N+1)]
 
-      for i in range (start, end):
-        val_left = result (dp, start, i-1)
-        val_right = result (dp, i+1, end)
-        val = max (val_left, val_right) + i
+  dp[0] = 0
+  dp_dict [0] = 0
+  dp[1] = 0
+  dp_dict [1] = 0
+  dp[2] = 1
+  dp_dict [2] = 1
+  dp[3] = 2
+  dp_dict [3] = 1
+  dp[4] = 4
+  dp_dict [4] = 2
 
-        if dp[start][end] == -1:
-          dp[start][end] = val
-        else:
-          dp[start][end] = min (dp[start][end], val)
+  for i in range (4, N+1):
+    # i - 3 is the split point
+    val_left = dp[i-4] + i - 3
+    val_right = i - 3 + i - 1
+    num = 0
+    val = 0
+    if val_left > val_right:
+      val = val_left
+      num = dp_dict[i-4]+1
+    else:
+      val = val_right
+      num = 2
 
-      return dp[start][end]
+    for j in range (i-4, 4, -1):
+      # j is the split point
+      val_left = dp[j-1] + j
+      val_right = dp[i-j+1] + dp_dict[i-j+1] * (j - 1)
+      if val_left > val_right:
+        new_val = val_left
+        n = dp_dict[j-1]+1
+      else:
+        new_val = val_right
+        n = dp_dict[i-j]
+      if new_val > val:
+        val = new_val
+        num = n
+
+    dp[i] = val
+    dp_dict[i] = num
+    print i, dp[i]
+
+  return dp[N]
 
 
-    if n == 0 or n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    elif n == 3:
-        return 2
-    elif n == 4:
-        return 4
-    elif n == 5:
-        return 6
-    dp = [[-1 for i in range (n+1)] for i in range (n+1)]
-    dp[0][0] = 0
-    dp[0][1] = 0
-    dp[0][2] = 1
-    dp[0][3] = 2
-    dp[0][4] = 4
-    dp[0][5] = 6
-
-    res = result(dp, 1, n)
-    return res
-
-solution = Solution ()
-print solution.getMoneyAmount (21)
+while True:
+  n= input ("give N: ")
+  getMoneyAmount (int (n))
