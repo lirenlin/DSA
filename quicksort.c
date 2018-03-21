298aa0572ebaa220848b4b3c4612676001788c30
Renlin Li <renlin.li@gmail.com>
2018-03-21 23:36:46 +0000

    update

--- quicksort.c
+++ quicksort.c
@@ -7,13 +7,33 @@ void swap (int *a, int *b)
   *b = tmp;
 }
 
+int partitionNew (int *array, int low, int high)
+{
+  int pivot = array[high];
+  int j = high;
+  int i = low;
+
+  while (1)
+    {
+      while (array[j] > pivot)
+	j--;
+      while (array[i] < pivot)
+	i++;
+
+      if (i >= j)
+	return i;
+
+      swap (array + i, array + j);
+    }
+}
+
 int partition (int *array, int low, int high)
 {
   int pivot = array[high];
   int i = low - 1;
   int j;
 
-  for (j = low; j < high; ++j)
+  for (j = low; j <= high; ++j)
     {
       if (array[j] <= pivot)
 	{
@@ -21,13 +41,16 @@ int partition (int *array, int low, int high)
 	  swap (array + i, array + j);
 	}
     }
-  swap (array + i + 1, array + high);
-  return i + 1;
+  return i;
 }
 
 void quickSort (int *array, int low, int high)
 {
-  int pi = partition (array, low, high);
+  if (low >= high)
+    return;
+
+  //int pi = partition (array, low, high);
+  int pi = partitionNew (array, low, high);
 
   quickSort (array, low, pi - 1);
   quickSort (array, pi + 1, high);
