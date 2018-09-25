896f8f8e1d725588dec28a479bddf21d5d4f7e44
Renlin Li <renlin.li@gmail.com>
2018-09-25 01:35:29 +0100

    priority queue

---
+++ priority_queue.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+
+#define MAX 10
+typedef struct
+{
+  int key;
+  int data;
+} Node;
+
+typedef struct
+{
+  unsigned size;
+  Node *root[MAX];
+  unsigned max;
+
+} NodeHeap;
+
+unsigned cmp (NodeHeap *heap, int idx1, int idx2)
+{
+  return (heap->root[idx1]->key > heap->root[idx2]->key);
+}
+
+void initHeap (NodeHeap *heap)
+{
+  heap->size = 0;
+  heap->max = MAX;
+}
+
+void swapNode (NodeHeap *heap, unsigned idx1, unsigned idx2)
+{
+  Node *tmp = heap->root[idx1];
+  heap->root[idx1] = heap->root[idx2];
+  heap->root[idx2] = tmp;
+}
+
+// Max node between parent and two children
+void swapUp (NodeHeap *heap, unsigned child_idx)
+{
+  if (child_idx == 0)
+    return;
+
+  do
+    {
+      unsigned p_idx = (child_idx - 1) / 2;
+      if (cmp (heap, child_idx, p_idx))
+	{
+	  swapNode (heap, child_idx, p_idx);
+	  child_idx = p_idx;
+	}
+      else
+	break;
+    } while (child_idx != 0);
+}
+
+// Max node between parent and two children
+void swapDown (NodeHeap *heap, unsigned parent_idx)
+{
+  unsigned p_idx = parent_idx;
+  unsigned l_idx = p_idx * 2 + 1;
+  unsigned r_idx = l_idx + 1;
+
+  while (1)
+    {
+      // No child node
+      if (l_idx >= heap->size)
+	break;
+
+      // single left child node
+      if (l_idx == heap->size - 1)
+	{
+	  if (cmp (heap, l_idx, p_idx))
+	    swapNode (heap, l_idx, p_idx);
+	  break;
+	}
+
+      // two children
+      unsigned cmp1 = cmp (heap, l_idx, r_idx);
+      unsigned cmp2 = cmp (heap, l_idx, p_idx);
+      unsigned cmp3 = cmp (heap, r_idx, p_idx);
+
+      if (cmp1 && cmp2)
+	{
+	  swapNode (heap, l_idx, p_idx);
+	  p_idx = l_idx;
+	}
+      else if (!cmp1 && cmp3)
+	{
+	  swapNode (heap, r_idx, p_idx);
+	  p_idx = r_idx;
+	}
+      else
+	break;
+
+      l_idx = p_idx * 2 + 1;
+      r_idx = l_idx + 1;
+    }
+}
+
+
+void dumpHeap (NodeHeap *heap)
+{
+  for (unsigned i = 0; i < heap->size; ++i)
+    printf ("%d: %d,", heap->root[i]->key, heap->root[i]->data);
+  printf ("\n");
+}
+
+void insert (NodeHeap *heap, Node *node)
+{
+  unsigned idx = heap->size;
+  heap->root[idx] = node;
+  heap->size += 1;
+
+  swapUp (heap, idx);
+}
+
+Node *removeMax (NodeHeap *heap)
+{
+  if (heap->size == 0)
+    return NULL;
+
+  unsigned last_idx = heap->size - 1;
+  heap->size = last_idx;
+
+  Node *res = heap->root[0];
+  heap->root[0] = heap->root[last_idx];
+  heap->root[last_idx] = NULL;
+
+  swapDown (heap, 0);
+}
+
+void main ()
+{
+  NodeHeap heap;
+  initHeap (&heap);
+
+  Node node1 = {10, 10};
+  Node node2 = {0, 0};
+  Node node3 = {1, 1};
+  Node node4 = {2, 2};
+  Node node5 = {3, 3};
+  Node node6 = {5, 5};
+  Node node7 = {4, 4};
+
+  insert (&heap, &node2);
+  insert (&heap, &node3);
+  insert (&heap, &node4);
+  insert (&heap, &node5);
+  insert (&heap, &node1);
+  insert (&heap, &node6);
+  dumpHeap (&heap);
+  removeMax (&heap);
+  dumpHeap (&heap);
+  insert (&heap, &node7);
+  dumpHeap (&heap);
+  removeMax (&heap);
+  dumpHeap (&heap);
+
+}
